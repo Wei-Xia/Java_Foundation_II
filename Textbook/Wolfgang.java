be7d6f7ed6726a51b70d614b8ebc8da24ba68377
Wei Xia <xw900812@gmail.com>
2015-08-30 22:34:58 -0400

    Chapter 2

--- Textbook/Wolfgang.java
+++ Textbook/Wolfgang.java
@@ -6,7 +6,7 @@ public class Wolfgang
 
 			System.out.print("The works of Wolfgang\ninclude ");
 			System.out.print("the following");
-			System.out.print("\nThe Turkish March" );
+			System.out.print("\nThe Turkish March " );
 			System.out.print("and Symphony No. 40");
 			System.out.println("in G minor.");
 
