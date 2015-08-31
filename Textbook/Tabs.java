0a8c805ee695b2f058b0eea6f5fccda94b8eceb3
Wei Xia <xw900812@gmail.com>
2015-08-30 22:28:48 -0400

    Chapter 2

--- Textbook/Tabs.java
+++ Textbook/Tabs.java
@@ -3,5 +3,7 @@ public class Tabs
 	public static void main(String[] args) {
 		
 		System.out.print("These are our top sellers:\n");
+		System.out.print("\tComputer game \n\tCoffee\n");
+		System.out.print("\tAspirin\n");
 	}
 }
