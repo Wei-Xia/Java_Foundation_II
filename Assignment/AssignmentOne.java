e727cd5301726cbc64c411b490a40eafcade53b1
Wei Xia <xw900812@gmail.com>
2015-09-02 13:37:43 -0400

    update

--- Assignment/AssignmentOne.java
+++ Assignment/AssignmentOne.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
 public class AssignmentOne {
-	
+
 	public static void main(String[] args) {
-		
+
+		System.out.println("Please enter i,j,k,n");
+		int i,j,k,n;
+		Scanner scan = new Scanner (System.in);
+		i = scan.nextInt();
+		j = scan.nextInt();
+		k = scan.nextInt();
+		n = scan.nextInt();
+
+		System.out.println(i,j,k,n);
+
 	}
 }
-	
