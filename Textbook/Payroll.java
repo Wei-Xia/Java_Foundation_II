42029f0c5012ad84304907c0d9496652386da1d8
Wei Xia <xw900812@gmail.com>
2015-09-04 08:28:56 -0400

    Chapter 2

---
+++ Textbook/Payroll.java
@@ -0,0 +1,24 @@
+import java.until.Scanner;   // Needed for the Scanner class
+
+/**
+  This program demonstrates the Scanner class
+*/
+
+public class Payroll {
+
+  public static void main(String[] args) {
+
+    String name;           // To hold a name
+    int hours;             // Hours worked
+    double payRate;        // Hourly pay rate
+    double grossPay;       // Gross pay
+
+    // Create a Scanner object to read input
+    Scanner keyboard = new Scanner(System.in);
+
+    // Get the user's name
+    System.out.print("What's your name?")
+    name = keyboard.nextLine();
+    
+  }
+}
