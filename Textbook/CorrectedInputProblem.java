646ab577a5e0dc04765fe895bdb64b48430578ee
Wei Xia <xw900812@gmail.com>
2015-09-04 08:54:48 -0400

    Chapter 2

--- Textbook/CorrectedInputProblem.java
+++ Textbook/CorrectedInputProblem.java
@@ -34,3 +34,15 @@ public class CorrectedInputProblem{
         System.out.println("Hello, " + name + ". Your age is " + age + " and your income is $" + income);
       }
 }
+
+/* Notes:
+Notice that after the user’s income is read by the nextDouble method in line 24,
+the nextLine method is called in line 27. The purpose of this call is to
+consume, or remove, the newline character that remains in the keyboard buffer.
+Then, in line 31, the nextLine method is called again. This time it correctly reads the user’s name.
+
+Notice that in line 27, where we consume the remaining newline character,
+we do not assign the method’s return value to any variable.
+This is because we are simply calling the method to remove the newline character, 
+and we do not need to keep the method’s return value.
+*/
