bfb8158942788b16f4229362e7f7f20ad48e4a5e
Wei Xia <xw900812@gmail.com>
2015-09-07 19:07:05 -0400

    Chapter 2

---
+++ Textbook/Chapter 2/PayrollDialog.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+/**
+  This program demonstrates using dialogs with JOptionPane.
+*/
+
+public class PayrollDialog
+{
+  public static void main(String[] args) {
+
+      String inputString;       // For reading input
+      String name;              // The user's name
+      int hours;                // The number of hours worked
+      double payRate;           // The user's hourly pay rate
+      double grossPay;          // The user's gross pay
+
+      // Get the user's name
+      name = JOptionPane.showInputDialog("What is " + "your name?");
+
+      
+  }
+}
