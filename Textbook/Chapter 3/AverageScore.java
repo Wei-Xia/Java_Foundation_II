3edfd533ea4aecffef0b28239f2bf199e40e5367
Wei Xia <xw900812@gmail.com>
2015-09-07 19:50:57 -0400

    Chapter 3

---
+++ Textbook/Chapter 3/AverageScore.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;    // Needed for JOptionPane
+
+/**
+  This program demonstrates the if statement
+*/
+
+public class AverageScore
+{
+  public static void main(String[] args) {
+
+      double score1;
+      double score2;
+      double score3;
+      double average;
+      String input;
+
+      // Get the first test score.
+      input = JOptionPane.showInputDialog("Enther score #1:");
+      score1 = double.parseDouble(input);
+
+      // Get the second test score.
+      input = JOptionPane.showInputDialog("Enther score #2:");
+      score2 = double.parseDouble(input);
+
+      // Get the third test score.
+      input = JOptionPane.showInputDialog("Enther score #3:");
+      score3 = double.parseDouble(input);
+
+      // Calculate the average score.
+      average = (score1+score2+score3)/3.0;
+
+      // Display the average score
+      JOptionPane.showMessageDialog(null, "The average is " + average);
+
+      // If the score was greater than 95, let the user know that's a great score
+      if (average>95)
+        JOptionPane.showMessageDialog(null, "That's a great score!")
+
+      System.exit(0);
+  }
+}
