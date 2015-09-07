ce180581be8eec5c0c9907982fe059c314e62675
Wei Xia <xw900812@gmail.com>
2015-09-07 19:03:12 -0400

    Chapter 2

--- Textbook/Chapter 2/NamesDialog.java
+++ Textbook/Chapter 2/NamesDialog.java
@@ -12,6 +12,16 @@ public class NamesDialog
     String middleName;    // The user's middle name
     String lastName;      // The user's last name
 
-    // Get
+    // Get the user's first name
+    firstName = JOptionPane.showInputDialog("What is "+"your first name?");
+
+    // Get the user's middle name
+    middleName = JOptionPane.showInputDialog("What is "+"your middle name?");
+
+    // Get the user's last name
+    lastName = JOptionPane.showInputDialog("What is "+"your last name?");
+
+    // Display a greeting
+    JOptionPane.showMessageDialog(null, "Hello "+firstName+" "+middleName+" "+lastName);
   }
 }
