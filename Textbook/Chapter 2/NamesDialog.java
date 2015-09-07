d1356b28e778669e80bc6fe7eeb2da019d3bc7cf
Wei Xia <xw900812@gmail.com>
2015-09-07 18:59:41 -0400

    Chapter 2

---
+++ Textbook/Chapter 2/NamesDialog.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+/**
+  This program demonstrates using dialogs with JOptionPane.
+*/
+
+public class NamesDialog
+{
+  public static void main(String[] args) {
+
+    String firstName;     // The user's first name
+    String middleName;    // The user's middle name
+    String lastName;      // The user's last name
+
+    // Get
+  }
+}
