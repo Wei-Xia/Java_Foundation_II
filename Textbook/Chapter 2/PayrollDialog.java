6a2ec583de6bdb062c1c97dba0b027da939e7d4a
Wei Xia <xw900812@gmail.com>
2015-09-07 19:12:40 -0400

    Chapter 2

--- Textbook/Chapter 2/PayrollDialog.java
+++ Textbook/Chapter 2/PayrollDialog.java
@@ -15,8 +15,27 @@ public class PayrollDialog
       double grossPay;          // The user's gross pay
 
       // Get the user's name
-      name = JOptionPane.showInputDialog("What is " + "your name?");
+      name = JOptionPane.showInputDialog("What is your name?");
 
-      
+      // Get the hours worked
+      inputString = JOptionPane.showInputDialog("How many hours did you work this week?");
+
+      // Convert the input to an int
+      hours = Integer.parseInt(inputString);
+
+      // Get the hourly pay rate
+      inputString = JOptionPane.showInputDialog("What is your hourly pay rate?");
+
+      // Convert the input to a double
+      payRate = Double.parseDouble(inputString);
+
+      // Calculate the gross pay
+      grossPay = hours * payRate;
+
+      // Display the results
+      JOptionPane.showMessageDialog(null, "Hello " + name + ". Your gross pay is $" + grossPay);
+
+      // End the program
+      System.exit(0);
   }
 }
