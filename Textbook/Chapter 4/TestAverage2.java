5016a08c60d9a8081b9f7c61c533b7a7192a0359
Wei Xia <xw900812@gmail.com>
2015-09-08 19:45:00 -0400

    Chapter 4

---
+++ Textbook/Chapter 4/TestAverage2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+/**
+This program demonstrates a nested loop.
+*/
+
+public class TestAverage2
+{
+  public static void main(String[] args) {
+
+    int numStudents,    // Number of students
+        numTests,       // Number of tests per students
+        score,          // Test score
+        total;          // Accumulator for test scores
+    double average;     // Average test score
+
+    // Create a Scanner object for keyboard input
+    Scanner keyboard = new Scanner(System.in);
+
+    // Get the number of students
+    System.out.print("How many students do you have? ");
+    numStudents = keyboard.nextInt();
+
+    // Get the number of test scores per students.
+    System.out.print("How many test scores per student? ");
+
+  }
+}
