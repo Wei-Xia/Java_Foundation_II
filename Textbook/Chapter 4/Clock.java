7f85b8e6eb8216972d27669f49da65e42e0d079f
Wei Xia <xw900812@gmail.com>
2015-09-08 19:38:04 -0400

    Chapter 4

--- Textbook/Chapter 4/Clock.java
+++ Textbook/Chapter 4/Clock.java
@@ -7,7 +7,7 @@ public class Clock
   public static void main(String[] args) {
 
     // simulate the clock.
-    for (int hours = 1;  hours <= 12; hours++) {
+    for (int hours = 1;  hours <= 1; hours++) {
       for (int minutes = 0; minutes <=59; minutes++ ) {
         for (int seconds = 0; seconds <=59; seconds++) {
           System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
