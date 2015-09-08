28b6b2ce768559d0a2a5649ad7e28d3cea253fbf
Wei Xia <xw900812@gmail.com>
2015-09-08 09:56:29 -0400

    Chapter 3

--- Textbook/Chapter 3/LeftJustified.java
+++ Textbook/Chapter 3/LeftJustified.java
@@ -17,7 +17,19 @@ public class LeftJustified
     // Display each variables left-justified in a field of 8 spaces
     System.out.printf("%-8d%-8d\n", num1, num2);
     System.out.printf("%-8d%-8d\n", num3, num4);
-    System.out.printf("%-8d%-8d\n", num5, num6);
+    System.out.printf("%-8d%-8d\n\n", num5, num6);
+
+    // Declare a variety of String variables
+    String name1 = "George";
+    String name2 = "Franklin";
+    String name3 = "Jay";
+    String name4 = "Ozzy";
+    String name5 = "Carmine";
+    String name6 = "Dee";
+
+    System.out.printf("%-10s%-10s\n", name1, name2);
+    System.out.printf("%-10s%-10s\n", name3, name4);
+    System.out.printf("%-10s%-10s\n", name5, name6);
 
   }
 }
