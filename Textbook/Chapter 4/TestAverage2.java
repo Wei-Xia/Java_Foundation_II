27e8e6b9a5cf619cb145bce265b601e6fb791d39
Wei Xia <xw900812@gmail.com>
2015-09-08 19:49:07 -0400

    Chapter 4

--- Textbook/Chapter 4/TestAverage2.java
+++ Textbook/Chapter 4/TestAverage2.java
@@ -23,6 +23,26 @@ public class TestAverage2
 
     // Get the number of test scores per students.
     System.out.print("How many test scores per student? ");
+    numTests = keyboard.nextInt();
+
+    // Process all the students
+    for (int student = 1; student <= numStudents; student++) {
+      total = 0;   // Set the accumulator to zero.
+
+      // Get the test scores for a student
+      System.out.println("Student number " + student);
+      System.out.println("-----------------------");
+
+      for (int test = 1; test <= numTests; test++) {
+        System.out.print("Enter score " + test + ": ");
+        score = keyboard.nextInt();
+        total += score;    // Add score to total
+      }
+
+      // Calculate and display the average
+      average = total / numTests;
+      System.out.printf("The average for student %d is %.1f. \n\n", student, average);
+    }
 
   }
 }
