f57d3400a11db441a9d45ba571fa70de8a6b65a1
Wei Xia <xw900812@gmail.com>
2015-09-16 23:11:56 -0400

    Chapter 5

--- Textbook/Chapter 5/ValueReturn.java
+++ Textbook/Chapter 5/ValueReturn.java
@@ -14,6 +14,23 @@ public class ValueReturn
 
     // Display the contents of all these variables
     System.out.println("The sum of " + value1 " and " + value2 + " is " + total);
+  }
 
+  /**
+  The sum method returns the sum of its two parameters.
+  @param num1 The first number to be added
+  @param num2 The second number to be added
+  @return The sum of num1 and num2.
+  */
+
+  public static int sum(int num1, int num2)
+  {
+    int result;
+
+    // Assign the value of num1 + num2 to result
+    result = num1+num2;
+
+    // Return the value in the result variable
+    return result;
   }
 }
