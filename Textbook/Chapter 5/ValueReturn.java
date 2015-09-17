aff29d14c53bfe10b13687ddb946f5da265bfe13
Wei Xia <xw900812@gmail.com>
2015-09-16 23:08:27 -0400

    Chapter 5

--- Textbook/Chapter 5/ValueReturn.java
+++ Textbook/Chapter 5/ValueReturn.java
@@ -4,5 +4,16 @@ This program demonstrates a value-returning method
 
 public class ValueReturn
 {
-  
+  public static void main(String[] args) {
+
+    int total, value1 = 20, value2 = 40;
+
+    // Call the sum method, passing the contents of value1 and value2 as arguments.
+    // Assign the return value to the total variable.
+    total = sum(value1, value2);
+
+    // Display the contents of all these variables
+    System.out.println("The sum of " + value1 " and " + value2 + " is " + total);
+
+  }
 }
