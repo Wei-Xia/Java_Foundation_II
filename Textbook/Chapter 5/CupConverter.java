a6335cebd28718d4614736f6c4dcb77db2dd44f8
Wei Xia <xw900812@gmail.com>
2015-09-18 19:40:00 -0400

    Chapter 5

--- Textbook/Chapter 5/CupConverter.java
+++ Textbook/Chapter 5/CupConverter.java
@@ -7,6 +7,19 @@ This program converts cups to fluid ounces.
 public class CupConverter
 {
   public static void main(String[] args) {
+
+    double cups;
+    double ounces;
+
+    // Get the number of cups
+    cups = getCups();
+
+    // Convert the cups to fluid ounces
+    ounces = cupsToOunces(cups);
+
+    // Display the result;
+    displayResults(cups, ounces);
+    System.exit(0);
     
   }
 }
