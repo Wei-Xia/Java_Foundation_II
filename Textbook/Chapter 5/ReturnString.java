d7bdf624ee150fb30ce520c136a902e12dcc894b
Wei Xia <xw900812@gmail.com>
2015-09-17 23:38:41 -0400

    Chapter 5

--- Textbook/Chapter 5/ReturnString.java
+++ Textbook/Chapter 5/ReturnString.java
@@ -1,3 +1,14 @@
 /**
 This program demonstrates a method that returns a reference to a String object.
 */
+
+public class ReturnString
+{
+  public static void main(String[] args) {
+
+    String customerName;
+
+    customerName = fullName("John", "Martin");
+    System.out.println(customerName);
+  }
+}
