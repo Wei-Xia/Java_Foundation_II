c5a20f2eed614783752187921bb5e1941c0daa72
Wei Xia <xw900812@gmail.com>
2015-09-20 14:21:39 -0400

    Chapter 5

--- Textbook/Chapter 5/SalesReport.java
+++ Textbook/Chapter 5/SalesReport.java
@@ -1,3 +1,15 @@
 import java.util.*;           // For the Scanner class
 import java.io.*;             // For file I/O classes
 import javax.swing.JOptionPane;   // For the JOptionPane class
+
+/**
+This program opens a file containing the sales amounts for 30 days.
+It calculates and displays the total sales and average daily sales.
+*/
+
+public class SalesReport
+{
+  public static void main(String[] args) throws IOException{
+
+  }
+}
