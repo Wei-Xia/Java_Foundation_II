edc5dc9cedc95f8c54deb1a1be0d0e7b6d3b1236
Wei Xia <xw900812@gmail.com>
2015-09-20 14:24:26 -0400

    Chapter 5

--- Textbook/Chapter 5/SalesReport.java
+++ Textbook/Chapter 5/SalesReport.java
@@ -11,5 +11,24 @@ public class SalesReport
 {
   public static void main(String[] args) throws IOException{
 
+    final int NUM_DAYS = 30;  // Number of days of sales
+    String filename;          // The name of the file to open
+    double totalSales;        // Total sales for period
+    double averageSales;      // Average daily sales
+
+    // Get the name of the file
+    filename = getFileName();
+
+    // Get the total sales from the file
+    totalSales = getTotalSales(filename);
+
+    // calculate the average
+    averageSales = totalSales / NUM_DAYS;
+
+    // display the total and average
+    displayResults(totalSales, averageSales);
+
+    System.exit(0);
+
   }
 }
