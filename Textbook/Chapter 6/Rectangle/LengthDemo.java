63177e6086e753b5ba078c9c8fa338b5c0ec9d09
Wei Xia <xw900812@gmail.com>
2015-09-23 19:12:51 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/Rectangle/LengthDemo.java
@@ -0,0 +1,11 @@
+/**
+  This program demonstrates the Rectangle class's setLength method.
+*/
+
+public class LengthDemo
+{
+  public static void main(String[] args)
+  {
+      // Create a Rectangle object and assign its address to the box variable
+  }
+}
