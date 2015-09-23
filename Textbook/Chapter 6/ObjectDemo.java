2064a0d4f326c532b07eb1e728d5fbde9faa29b4
Wei Xia <xw900812@gmail.com>
2015-09-23 18:56:58 -0400

    Chapter 6

--- Textbook/Chapter 6/ObjectDemo.java
+++ Textbook/Chapter 6/ObjectDemo.java
@@ -1,2 +1,18 @@
 import java.util.*;         // Needed for the Scanner and Random class
 import java.io.*;           // Needed for file I/O classes
+
+/**
+This program writes random numbers to a file
+*/
+
+public class ObjectDemo
+{
+  public static void main(String[] args) throws IOException{
+
+    int maxNumbers;     // Max number of random numbers
+    int number;         // To hold a random number
+
+    // Create a Scanner object for keyboard input.
+    Scanner keyboard = new Scanner(System.in);
+  }
+}
