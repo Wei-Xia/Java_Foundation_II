35bd0e66fe5fb1592f8b8badb16a23177e7c64be
Wei Xia <xw900812@gmail.com>
2015-09-23 19:00:14 -0400

    Chapter 6

--- Textbook/Chapter 6/ObjectDemo.java
+++ Textbook/Chapter 6/ObjectDemo.java
@@ -14,5 +14,24 @@ public class ObjectDemo
 
     // Create a Scanner object for keyboard input.
     Scanner keyboard = new Scanner(System.in);
+
+    // Create a Random object to generate random numbers.
+    Random rand = new Random();
+
+    // Create a PrintWriter object to open the file
+    PrintWriter outputFile = new PrintWriter("numbers.txt");
+
+    // Get the number of random numbers to write
+    System.out.print("How many random numbers should I write? ");
+    maxNumbers = keyboard.nextInt();
+
+    // Write the random numbers to the file
+    for (int count = 0 ; count < maxNumbers; count++) {
+      // Generate a random integer.
+      number = rand.nextInt();
+
+      
+    }
+
   }
 }
