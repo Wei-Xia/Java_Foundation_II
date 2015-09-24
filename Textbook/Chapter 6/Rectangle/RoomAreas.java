02a541f2d25495b7ae736db764e7de2b8c07f5b7
Wei Xia <xw900812@gmail.com>
2015-09-24 08:39:33 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/Rectangle/RoomAreas.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+  This program creates three instances of the Rectangle class.
+*/
+
+public class RoomArea
+{
+  public static void main(String[] args)
+  {
+      double number;      // To hold a number
+      double totalArea;   // The total area
+      String input;       // To hold user input
+  }
+}
