a73c6cbc164eb6315ab084eb451459f7552ece89
Wei Xia <xw900812@gmail.com>
2015-09-24 08:25:41 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/Rectangle/LengthWidthDemo.java
@@ -0,0 +1,12 @@
+/**
+This program demonstrates the Rectangle class's setLength, setWidth, getLength, getWidth methods.
+*/
+
+public class LengthWidthDemo
+{
+  public static void main(String[] args)
+  {
+      // Create a Rectangle object
+      Rectangle box = new Rectangle();
+  }
+}
