b63c80267c9973276e0fd410935f72b6979b9eba
Wei Xia <xw900812@gmail.com>
2015-09-27 15:41:13 -0400

    Chapter 6

--- Textbook/Chapter 6/CellPhone/CellPhoneTest.java
+++ Textbook/Chapter 6/CellPhone/CellPhoneTest.java
@@ -19,6 +19,24 @@ public class CellPhoneTest
       System.out.print("Enter the manufacturer: ");
       testMan = keyboard.nextLine();
 
-      
+      // Get the model number
+      System.out.print("Enter the model number: ");
+      testMod = keyboard.nextLine();
+
+      // Get the retail price
+      System.out.print("Enter the retail price: ");
+      testPrice = keyboard.nextDouble();
+
+      // Create an instance of the CellPhone class,
+      // passing the data that was entered as arguments to the constructor
+      CellPhone phone = new CellPhone(testMan, testMod, testPrice);
+
+      // Get the data from the phone and display it
+      System.out.println();
+      System.out.println("Here is the data that you privoded:");
+      System.out.println("Manufacturer: " + phone.getManufact());
+      System.out.println("Model number: " + phone.getModel());
+      System.out.println("Retail Price: " + phone.getRetailPrice());
+
   }
 }
