27e063242194622bb63e6b2f9b240a8051942719
Wei Xia <xw900812@gmail.com>
2015-09-27 15:23:30 -0400

    Chapter 6

--- Textbook/Chapter 6/CellPhone/CellPhone.java
+++ Textbook/Chapter 6/CellPhone/CellPhone.java
@@ -15,5 +15,52 @@ public class CellPhone
     @param mod The phone's model
     @param price The phone's retail price
   */
-  
+
+  public CellPhone(String man, String mod, double price)
+  {
+    manufact = man;
+    model = mod;
+    retailPrice = price;
+
+    /**
+      The setManufact method sets the phone's manufacturer name.
+      @param man The phone's manufacturer
+    */
+
+    public void setManufact(String man)
+    {
+      manufact = man;
+    }
+
+    /**
+      The setModel method sets the phone's model number.
+      @param mod The phone's model number
+    */
+
+    public void setModel(String mod)
+    {
+      model = mod;
+    }
+
+    /**
+      The setRetailPrice method sets the phone's retail price
+      @param price The phone's retail price
+    */
+
+    public void setRetailPrice(double price)
+    {
+      retailPrice = price;
+    }
+
+    /**
+      getManufact method
+      @return The name of the phone's manufacturer
+    */
+
+    public String getManufact()
+    {
+      return manufact;
+    }
+
+  }
 }
