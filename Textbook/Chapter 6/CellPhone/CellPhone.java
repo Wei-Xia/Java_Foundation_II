cd63b372ce2d19ccb65cf08039d25b5b1ee45443
Wei Xia <xw900812@gmail.com>
2015-09-27 15:17:44 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/CellPhone/CellPhone.java
@@ -0,0 +1,19 @@
+/**
+  The CellPhone class holds data about a cell phone
+*/
+
+public class CellPhone
+{
+  // Fields
+  private String manufact;      // Manufacturer
+  private String model;         // Model
+  private double retailPrice;   // Retail price
+
+  /**
+    Constructor
+    @param man The phone's manufacturer.
+    @param mod The phone's model
+    @param price The phone's retail price
+  */
+  
+}
