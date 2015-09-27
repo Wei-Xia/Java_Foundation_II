3c88621952f2e740dc543c107d827e4022536771
Wei Xia <xw900812@gmail.com>
2015-09-27 19:21:32 -0400

    Chapter 6

--- Textbook/Chapter 6/Die.java/DieDemo.java
+++ Textbook/Chapter 6/Die.java/DieDemo.java
@@ -19,4 +19,38 @@ public class Die
     sides = numSides;
     roll();
   }
+
+  /**
+    The roll method simulates the rolling of the die
+  */
+
+  public void roll()
+  {
+    // Create a Random object.
+    Random rand = new Random();
+
+    // Get a random value for the die
+    value = rand.nextInt(sides) + 1;
+  }
+
+  /**
+    getSides method
+    @return The number of sides for this die
+  */
+
+  public int getSides()
+  {
+    return sides;
+  }
+
+  /**
+    getValue method
+    @return The value of the die
+  */
+
+  public int getValue()
+  {
+    return value;
+  }
+
 }
