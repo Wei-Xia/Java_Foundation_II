173c0aa98b129cf7bb36cc85455b70d7bd14795a
Wei Xia <xw900812@gmail.com>
2015-09-27 15:36:58 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/CellPhone/CellPhoneTest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+  This program runs a simple test of the CellPhone class
+*/
+
+public class CellPhoneTest
+{
+  public static void main(String[] args)
+  {
+      String testMan;
+      String testMod;
+      double testPrice;
+
+      // Create a Scanner object for keyboard input.
+      Scanner keyboard = new Scanner(System.in);
+
+      // Get the manufacturer name
+      System.out.print("Enter the manufacturer: ");
+      testMan = keyboard.nextLine();
+
+      
+  }
+}
