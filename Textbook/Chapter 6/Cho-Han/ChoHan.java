af1ef3a09f5c90adbdda8fe45d2e7d845b3b23c0
Wei Xia <xw900812@gmail.com>
2015-09-29 22:23:22 -0400

    Chapter 6

--- Textbook/Chapter 6/Cho-Han/ChoHan.java
+++ Textbook/Chapter 6/Cho-Han/ChoHan.java
@@ -10,5 +10,14 @@ public class ChoHan
 
       // Create a Scanner object for keyboard input
       Scanner keyboard = new Scanner(System.in);
+
+      // Get the player's name
+      System.out.print("Enter the first player's name: ");
+      player1Name = keyboard.nextLine();
+      System.out.print("Enter the second plyaer's name: ");
+      player2Name = keyboard.nextLine();
+
+      // Create the dealer
+      Dealer dealer = new Dealer();
   }
 }
