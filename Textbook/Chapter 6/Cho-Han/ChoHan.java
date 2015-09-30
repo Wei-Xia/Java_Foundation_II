a709f353dc26153a40e61e1ffd9c42fce9e9e869
Wei Xia <xw900812@gmail.com>
2015-09-29 22:20:42 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/Cho-Han/ChoHan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ChoHan
+{
+  public static void main(String[] args)
+  {
+      final int MAX_ROUNDS = 5;     // Number of rounds
+      String player1Name;           // First player's name
+      String player2Name;           // Second player's name
+
+      // Create a Scanner object for keyboard input
+      Scanner keyboard = new Scanner(System.in);
+  }
+}
