43fe4eca7bf84ed6279b98a776e0e9b4c91b57c7
Wei Xia <xw900812@gmail.com>
2015-10-03 13:49:52 -0400

    Chapter 7

--- Textbook/Chapter 7/ArrayInitialization.java
+++ Textbook/Chapter 7/ArrayInitialization.java
@@ -7,5 +7,10 @@ public class ArrayInitialization
   public static void main(String[] args)
   {
       int [] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+      for (int i = 0 ; i<days.length ; i++ )
+      {
+          System.out.println("Month " + (i+1) + " has " + days[i] + " day.");
+      }
   }
 }
