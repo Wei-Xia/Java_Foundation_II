ad30c0fcfdfa8611faf58a315ecfa45b6c044fb4
Wei Xia <xw900812@gmail.com>
2015-10-02 23:20:21 -0400

    Chapter 7

---
+++ Textbook/Chapter 7/ReturnArray.java
@@ -0,0 +1,13 @@
+/**
+  This program demonstrates how a reference to an array can be returned from a method
+*/
+
+public class ReturnArray
+{
+  public static void main(String[] args)
+  {
+      double [] values;
+
+      values = getArray();
+  }
+}
