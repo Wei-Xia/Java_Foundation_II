f75fb5a8cb3ea44c55046c2f60c057e32f3c9f74
Wei Xia <xw900812@gmail.com>
2015-10-03 13:37:47 -0400

    Chapter 7

--- Textbook/Chapter 7/ArrayDemo2.java
+++ Textbook/Chapter 7/ArrayDemo2.java
@@ -8,6 +8,12 @@ public class ArrayDemo2
 {
   public static void main(String[] args)
   {
+    final int EMPLOYEES = 3;                // Number of employees
+    int [] hours = new int[EMPLOYEES];      // Array of hours
 
+    // Create a Scanner object for keyboard input
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Enter the hours worked by " + EMPLOYEES + " employees.");
   }
 }
