da0adff5caf97e925f5ff178239a46f09af6e744
Wei Xia <xw900812@gmail.com>
2015-10-03 13:48:38 -0400

    Chapter 7

---
+++ Textbook/Chapter 7/ArrayInitialization.java
@@ -0,0 +1,11 @@
+/**
+  This program shows an array being initialized
+*/
+
+public class ArrayInitialization
+{
+  public static void main(String[] args)
+  {
+      int [] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  }
+}
