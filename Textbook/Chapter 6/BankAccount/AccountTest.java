fc6cc364c01dd0830e5930760412e720313fe873
Wei Xia <xw900812@gmail.com>
2015-10-04 18:51:27 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/BankAccount/AccountTest.java
@@ -0,0 +1,13 @@
+import javax.swing.*;       // For the JOptionPane class
+
+/**
+  This program demonstrates the BankAccount class.
+*/
+
+public class AccountTest
+{
+  public static void main(String[] args)
+  {
+      String input;     // To hold user input
+  }
+}
