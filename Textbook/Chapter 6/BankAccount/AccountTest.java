3f1549279001058fb24d4dc72a7e43115376a873
Wei Xia <xw900812@gmail.com>
2015-10-04 18:53:52 -0400

    Chapter 6

--- Textbook/Chapter 6/BankAccount/AccountTest.java
+++ Textbook/Chapter 6/BankAccount/AccountTest.java
@@ -9,5 +9,17 @@ public class AccountTest
   public static void main(String[] args)
   {
       String input;     // To hold user input
+
+      // Get the starting object
+      input = JOptionPane.showInputDialog("What is your account's starting balance?");
+
+      // Create a BankAccount object
+      BankAccount account = new BankAccount(input);
+
+      // Get the amount of pay.
+      input = JOptionPane.showInputDialog("How much were you paid this month?");
+
+      // Deposit the user's pay into the account
+      account.deposit(input);
   }
 }
