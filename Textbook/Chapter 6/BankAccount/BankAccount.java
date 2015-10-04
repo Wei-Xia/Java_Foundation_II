fa64dcdcf95ad54793732b5887d42ef45c670ea6
Wei Xia <xw900812@gmail.com>
2015-10-04 18:30:42 -0400

    Chapter 6

---
+++ Textbook/Chapter 6/BankAccount/BankAccount.java
@@ -0,0 +1,17 @@
+/**
+  The BankAccount class simulates a bank account.
+*/
+
+public class BankAccount
+{
+  private double balance;     // Account balance
+
+  /**
+    This constructor sets the starting balance at 0.0;
+  */
+
+  public BankAccount()
+  {
+    balance = 0.0;
+  }
+}
