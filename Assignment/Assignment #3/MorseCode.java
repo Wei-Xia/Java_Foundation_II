0814c545926b999372fa854af94c4af27ebf3529
Wei Xia <xw900812@gmail.com>
2015-10-10 17:16:30 -0400

    Assignment #3

--- Assignment/Assignment #3/MorseCode.java
+++ Assignment/Assignment #3/MorseCode.java
@@ -8,33 +8,28 @@ class MorseCode
     char [] English = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                        'Y', 'Z'};
+                        'Y', 'Z',' '};
 
     String [] Morse = { "·-", "-···", "-·-·", "-··", "·", "··-·", "--·", "····",
                         "··", "·---", "-·-", "·-··", "--", "-·", "---", "·--·",
                         "--·-",  "·-·", "···", "-", "··-", "···-", "·--", "-··-",
-                        "-·--", "--··"};
+                        "-·--", "--··","bb"};
 
     String sentence;
-
-    System.out.println("Please enter a sentence in English, or enter $ to quit");
-
     Scanner keyboard = new Scanner(System.in);
-    sentence = keyboard.nextLine();
 
-    sentence = sentence.toUpperCase();
-
-    while(sentence != "$")
-    {
+      System.out.println("Please enter a sentence in English, or enter $ to quit");
+      sentence = keyboard.nextLine();
+      sentence = sentence.toUpperCase();
       for ( int a = 0; a < English.length; a++ )
       {
         for ( int b = 0; b < sentence.length (); b++ )
         {
-          if ( English [ a ] == sentence.charAt ( b ) )
-          System.out.printf(Morse[a]);
+          if ( English[a] == sentence.charAt(b))
+          System.out.printf(Morse[a]+"b");
         }
       }
-    }
+
 
   }
 }
