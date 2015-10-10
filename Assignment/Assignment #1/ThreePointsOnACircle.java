a5ef0283bd57012e3f2bc750fd949430d33ca205
Wei Xia <xw900812@gmail.com>
2015-10-10 17:16:39 -0400

    Professor Version

---
+++ Assignment/Assignment #1/ThreePointsOnACircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ThreePointsOnACircle
+
+	{
+		/* Determine if k is on the arc when moving clockwise from j to i on a circle of size n. */
+
+		public static void main(String[] args)
+			{
+			}
+	}
