c90870a370a5b0d359db2862570e17d71a1e063f
Wei Xia <xw900812@gmail.com>
2015-10-10 23:13:30 -0400

    AssignmentTwo

---
+++ Assignment/Assignment #2/AssignmentTwo.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class ThreePointsOnACircle
+	{
+		/* Determine if k is on the arc when moving clockwise from j to i on a circle of size n. */
+
+		public static void main(String[] args)
+			{
+				Scanner keyboard = new Scanner(System.in);			// Scanner object wrapped around the keyboard, for user input
+				int n,i,j,k;					// n is size of circle, and i, j and k are the circular coordinates of 3 points on it
+
+				// prompt user for values of n,i,j, and k; input values after each prompt
+				System.out.print("\nEnter size of circle(n): ");
+				n = keyboard.nextInt();
+
+				System.out.print("\nEnter first point(i): ");
+				i = keyboard.nextInt();
+
+				System.out.print("\nEnter second point(j): ");
+				j = keyboard.nextInt();
+
+				System.out.print("\nEnter third points(k): ");
+				k = keyboard.nextInt();
+
+				// Determine if n, i, j and k satisfy all 4 necessary conditions and display appropriate message
+				if (n>0 && i>=0 && i<=n && j>=0 && j<=n && k>=0 && k<=n && i!=j &&
+						((k<=i&&i<j)||(i<j&&j<k)||(j<k&&k<=i)))
+					System.out.println("\n\nk lies on the arc from j to i when moving clockwise on a circle of size n.");
+				else
+					System.out.println("\n\nk does not lie on the arc from j to i when moving clockwise on a circle of size n.");
+
+			}
+
+		static int getAndVerfyInput (String userPrompt, int lowerBound, int upperBound)
+			{
+				
+			}
+	}
