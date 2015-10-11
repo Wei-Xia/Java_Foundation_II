d4a8f2a5bff0a4b14d0834b96239d4633512c3db
Wei Xia <xw900812@gmail.com>
2015-10-11 15:36:13 -0400

    Assignment #2

--- Assignment/Assignment #2/AssignmentTwo.java
+++ Assignment/Assignment #2/AssignmentTwo.java
@@ -8,7 +8,6 @@ class AssignmentTwo
 			{
 				int n,i,j,k;					// n is size of circle, and i, j and k are the circular coordinates of 3 points on it
 
-
 				n = getAndVerfyInput("Enter size of circle, n", 0, Integer.MAX_VALUE);
 				i = getAndVerfyInput("Enter first point(i)", 0, n);
 				j = getAndVerfyInput("Enter second point(j)", 0, n);
@@ -17,12 +16,14 @@ class AssignmentTwo
 				// Determine if n, i, j and k satisfy all 4 necessary conditions and display appropriate message
 				if (n>0 && i>=0 && i<=n && j>=0 && j<=n && k>=0 && k<=n && i!=j &&
 						((k<=i&&i<j)||(i<j&&j<k)||(j<k&&k<=i)))
-					System.out.println("\nk lies on the arc from j to i when moving clockwise on a circle of size n.");
+					System.out.println("\nk lies on the arc from j to i when moving clockwise
+					 									on a circle of size n.");
 				else
-					System.out.println("\nk does not lie on the arc from j to i when moving clockwise on a circle of size n.");
+					System.out.println("\nk does not lie on the arc from j to i
+														when moving clockwise on a circle of size n.");
 			}
 
-		static int getAndVerfyInput (String userPrompt, int lowerBound, int upperBound)
+		static int getAndVerfyInput(String userPrompt, int lowerBound, int upperBound)
 			{
 				System.out.println(userPrompt);
 
