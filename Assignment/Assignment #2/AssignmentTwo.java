c8fec21f4fde6af7bd7d220f1c25a1479eb916d9
Wei Xia <xw900812@gmail.com>
2015-10-12 15:27:09 -0400

    Assignment #2

--- Assignment/Assignment #2/AssignmentTwo.java
+++ Assignment/Assignment #2/AssignmentTwo.java
@@ -16,11 +16,9 @@ class AssignmentTwo
 				// Determine if n, i, j and k satisfy all 4 necessary conditions and display appropriate message
 				if (n>0 && i>=0 && i<=n && j>=0 && j<=n && k>=0 && k<=n && i!=j &&
 						((k<=i&&i<j)||(i<j&&j<k)||(j<k&&k<=i)))
-					System.out.println("\nk lies on the arc from j to i when moving clockwise
-					 									on a circle of size n.");
+					System.out.println("\nk lies on the arc from j to i when moving clockwise on a circle of size n.");
 				else
-					System.out.println("\nk does not lie on the arc from j to i
-														when moving clockwise on a circle of size n.");
+					System.out.println("\nk does not lie on the arc from j to i when moving clockwise on a circle of size n.");
 			}
 
 		static int getAndVerfyInput(String userPrompt, int lowerBound, int upperBound)
@@ -31,6 +29,12 @@ class AssignmentTwo
 
 				Scanner keyboard = new Scanner(System.in);			// Scanner object wrapped around the keyboard, for user input
 				result = keyboard.nextInt();
+
+				while	(result > upperBound || result < lowerBound)
+				{
+					System.out.println(userPrompt);
+					result = keyboard.nextInt();
+				}
 
 				return result;
 			}
