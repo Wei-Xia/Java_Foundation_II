a9384a27eecb5204f98f2210c483621d8870c53d
Wei Xia <xw900812@gmail.com>
2015-10-12 15:08:13 -0400

    Assignment #3

--- Assignment/Assignment #3/AssignmentThree.java
+++ Assignment/Assignment #3/AssignmentThree.java
@@ -1,40 +1,54 @@
+// Assignment #3
+// CS 504
+// Wei
+
 import java.util.*;
 
-public class AssignmentThree{
+public class AssignmentThree
+{
 	public static void main (String [] args)
 	{
-		Scanner englishtomorse = new Scanner(System.in);
-		System.out.println ("Please enter an original sentence in English.");
-		english = englishtomorse.nextLine();
-		String str = englishtoMorse(english);
-		System.out.println(str);
-		englishtomorse.close();
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter an original sentence in English.");
+		String sentence = keyboard.nextLine();
+		System.out.println(convertToMorse(sentence));
 	}
 
-	public static String englishtoMorse(String english)
+	public static String convertToMorse(String sentence)
 	{
 		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String code[] =  {"._", "_...", "_._.", "_..",".",".._.","__.", "....",
 			"..", ".___", "_._", "._..", "__","_ .", "___", ".__.", "__._", "._.",
 			"...", "_", ".._", "..._", ".__", "_.._", "_.__", "__.."};
-		english = english.toUpperCase();
-		for(int xyz = 0; xyz < english.length(); xyz++)
+		sentence = sentence.toUpperCase();
+
+		while(!sentence.contains("$"))
 		{
-			char letter = english.charAt(xyz);
-			if (letter == ' ')
-			{
-				System.out.print ("ø");
-				continue;
-			}
-   			for(int variable = 0; variable < alphabet.length(); variable++)
-   			{
-       			if(alphabet.charAt(variable) == letter)
-       			{
-       				System.out.print(code[variable] + "ø");
-       				break;
-       			}
-   			}
+			for(int a = 0; a < sentence.length(); a++)
+				{
+					char letter = sentence.charAt(a);
+					if (letter == ' ')
+						{
+							System.out.print ("ø");
+							continue;
+						}
+   					for(int variable = 0; variable < alphabet.length(); variable++)
+   						{
+       						if(alphabet.charAt(variable) == letter)
+       							{
+       								System.out.print(code[variable] + "ø");
+       								break;
+       							}
+   						}
+				}
+		
+		System.out.println("\nPlease enter a sentence in English, or enter $ to quit");
+		
+		Scanner keyboard = new Scanner(System.in);
+		sentence = keyboard.nextLine();
+		sentence = sentence.toUpperCase();
 		}
+		
    		return " ";
 	}
 }
