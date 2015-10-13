f85e25520cc1e13701926d75edb0a0919e6ae848
Wei Xia <xw900812@gmail.com>
2015-10-12 23:11:03 -0400

    Assignment #4

--- Assignment/Assignment #4/AssignmentFour.java
+++ Assignment/Assignment #4/AssignmentFour.java
@@ -1,6 +1,20 @@
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
 class AssignmentFour
 {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args)
+  {
+    System.out.println("Enter a sentence:");
+    Scanner keyboard = new Scanner(System.in);
+     
+
+    StringTokenizer st = new StringTokenizer("this is a test");
+    while (st.hasMoreTokens())
+    {
+    System.out.println(st.nextToken());
+    }
+
+
   }
 }
