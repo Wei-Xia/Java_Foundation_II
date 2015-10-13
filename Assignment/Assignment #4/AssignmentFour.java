9471e391d2d389d7133b24a1ee8b6d918d1ac4cf
Wei Xia <xw900812@gmail.com>
2015-10-13 17:13:25 -0400

    Assignment #4

--- Assignment/Assignment #4/AssignmentFour.java
+++ Assignment/Assignment #4/AssignmentFour.java
@@ -5,14 +5,35 @@ class AssignmentFour
 {
   public static void main(String[] args)
   {
-    System.out.println("Enter a sentence:");
-    Scanner keyboard = new Scanner(System.in);
-
-
-    StringTokenizer st = new StringTokenizer("this is a test");
-    while (st.hasMoreTokens())
-    {
-    System.out.println(st.nextToken());
-    }
+	while (true)
+	{
+		System.out.println("Enter a sentence:");
+		Scanner keyboard = new Scanner(System.in);
+		String sentence = keyboard.nextLine();
+    
+		if (sentence.isEmpty())      // isEmpty is to decide whether user enters an empty string or not
+		{
+			break;
+		}
+		else 
+		{
+			StringTokenizer st = new StringTokenizer(sentence);
+			String[] arrayElement = new String
+   
+			while (st.hasMoreTokens())
+			{
+				for(int i = 0; i < arrayElement.length; i++)
+				{
+					arrayElement[index] = i;
+				}
+				
+				for (int i = 0; i < arrayElement.length; i++)
+				{
+					System.out.println(arrayElement[index]);
+				}
+				System.out.println(st.nextToken());
+			}
+		}
+	}
   }
 }
