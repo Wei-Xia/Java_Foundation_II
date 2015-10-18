d853878a45a7264f5d7033a9facc5dbecbad9d53
Wei Xia <xw900812@gmail.com>
2015-10-17 23:27:40 -0400

    Assignment #4

--- Assignment/Assignment #4/AssignmentFour.java
+++ Assignment/Assignment #4/AssignmentFour.java
@@ -10,29 +10,36 @@ class AssignmentFour
 		System.out.println("Enter a sentence:");
 		Scanner keyboard = new Scanner(System.in);
 		String sentence = keyboard.nextLine();
-    
+
 		if (sentence.isEmpty())      // isEmpty is to decide whether user enters an empty string or not
 		{
 			break;
 		}
-		else 
+		else
 		{
-			StringTokenizer st = new StringTokenizer(sentence);
-			String[] arrayElement = new String
-   
-			while (st.hasMoreTokens())
-			{
-				for(int i = 0; i < arrayElement.length; i++)
-				{
-					arrayElement[index] = i;
-				}
-				
-				for (int i = 0; i < arrayElement.length; i++)
-				{
-					System.out.println(arrayElement[index]);
-				}
-				System.out.println(st.nextToken());
-			}
+			// StringTokenizer st = new StringTokenizer(sentence);
+			// String[] arrayElement = new String();
+      //
+      // while (st.hasMoreTokens())
+		  //   {
+			// 	    for(int i = 0; i < arrayElement.length; i++)
+			// 	      {
+			// 		        arrayElement[index] = i;
+			// 	      }
+      //
+			// 	          for (int i = 0; i < arrayElement.length; i++)
+  		// 		            {
+			// 		       System.out.println(arrayElement[index]);
+			// 	              }
+      //
+      //   System.out.println(st.nextToken());
+		  //   }
+      StringTokenizer st = new StringTokenizer(sentence);
+
+      while (st.hasMoreTokens())
+      {
+        System.out.println(st.nextToken());
+      }
 		}
 	}
   }
