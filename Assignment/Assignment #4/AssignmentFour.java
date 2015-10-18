87a0aaada908a30d1a2d3b8015e627fc9938288a
Wei Xia <xw900812@gmail.com>
2015-10-18 00:26:05 -0400

    Assignment #4

--- Assignment/Assignment #4/AssignmentFour.java
+++ Assignment/Assignment #4/AssignmentFour.java
@@ -1,6 +1,6 @@
 import java.util.StringTokenizer;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 
 class AssignmentFour
@@ -23,27 +23,21 @@ class AssignmentFour
 		{
       StringTokenizer st = new StringTokenizer(sentence);
       // ArrayList sentenceElement = new ArrayList();
-      List<String> sentenceElement = new ArrayList<>();
+      List<String> sentenceElement = new ArrayList<String>();
 
 
       while (st.hasMoreTokens())
       {
-        System.out.println(st.nextToken());   // Output each words
-
+        // System.out.println(st.nextToken());   // Output each words
         sentenceElement.add(st.nextToken());
-
-
-        // String i = st.nextToken();
-        // for(int index = 0 ;index < arrayElement.length; index++)
-        // {
-        //   arrayElement[index]= i;
-        // }
       }
 
-      // for(int index = 0; index < arrayElement.length; index++)
-      // {
-      //   System.out.println(arrayElement[index]);
-      // }
+      for(int index = 0; index < sentenceElement.length; index++)
+      {
+        System.out.println(sentenceElement[index]);
+      }
+
+
 		}
 
 
