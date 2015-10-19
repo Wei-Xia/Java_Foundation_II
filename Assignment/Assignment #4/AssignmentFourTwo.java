c0f14d4d3b1ece437661cdb5dbdb7edd171eb5e5
Wei Xia <xw900812@gmail.com>
2015-10-19 16:50:24 -0400

    Assignment #4

--- Assignment/Assignment #4/AssignmentFourTwo.java
+++ Assignment/Assignment #4/AssignmentFourTwo.java
@@ -5,39 +5,41 @@ class AssignmentFourTwo
   public static void main(String[] args)
   {
     while (true)
-	  {
+	{
 		    System.out.println("Enter a sentence:");
 		    Scanner keyboard = new Scanner(System.in);
-        String[] myPhrase = keyboard.nextLine().split(" ");
+        	String[] sentence = keyboard.nextLine().split(" ");
 
-		    if (myPhrase.isEmpty())      // quit the program when user enter an empty string
+		    if (sentence.isEmpty())      
 		    {
-			        break;
+				break;	// quit the program when user enter an empty string
 		    }
-        else
+       		
+       		else
 		    {
-              HashMap<String, Integer> myWordsCount = new HashMap<String, Integer>();
+            	HashMap<String, Integer> myWordsCount = new HashMap<String, Integer>();
 
-              for (String s : myPhrase)
-              {
-                if (myWordsCount.containsKey(s))
-                    myWordsCount.replace(s, myWordsCount.get(s) + 1);
-                else
-                    myWordsCount.put(s, 1);
-              }
+            	for (String s : sentence)
+            		{
+                		if (myWordsCount.containsKey(s))
+                    	myWordsCount.replace(s, myWordsCount.get(s) + 1);
+                		else
+                    	myWordsCount.put(s, 1);
+              		}
 
-              // System.out.println(myWordsCount.getValue(s));     // Output the all HashMap to check it is correct
+//             	System.out.println(myWordsCount.getValue(s));     
+            		// Output the all HashMap to check it is correct
 
-              Iterator iterator = myWordsCount.keySet().iterator();
+    			Iterator iterator = myWordsCount.keySet().iterator();
 
-              while (iterator.hasNext())
-              {
-                  String key = iterator.next().toString();
-                  Integer value = myWordsCount.get(key);
+              	while (iterator.hasNext())
+              	{
+              		String key = iterator.next().toString();
+              		Integer value = myWordsCount.get(key);
 
-                  System.out.println(key + " " + value);
-              }
+              		System.out.println(key + "\t\t" + value);
+          		}
 		    }
-	  }
+	}
   }
 }
