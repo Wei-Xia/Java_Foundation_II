912bda7c831e34e71d417b716b0ad764989d90f5
Wei Xia <xw900812@gmail.com>
2015-11-10 10:14:01 -0500

    Assignment #3

---
+++ Assignment/Assignment #3/MorseCode_Prof.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class MorseCode_Prof
+  {
+    /* Prompt user for strings and display morse code equivalent until user enters sentinel */
+    public static void main(String[] args)
+      {
+        final String SENTINEL = "$";        // sentinel string that terminates program
+        Scanner keyboard = new Scanner(System.in);      // Scanner object wrapped around the keyboard, for user input
+        String [] morseCode 
+      }
+  }
