bbb1ce665d445c8155117c71d67df2268656af55
Wei Xia <xw900812@gmail.com>
2015-11-10 10:55:51 -0500

    Assignment #4

--- Assignment/Assignment #4/WordCount.java
+++ Assignment/Assignment #4/WordCount.java
@@ -14,30 +14,30 @@ class WordCount
         String input = keyboard.nextLine();
 
         // If input is not SENTINEL, display word count, and then prompt and input again and display again, repeatly
-        while (!input.equals(SENTINEL))
+        while (!input.equals(SENTINEL))         // SENTINEL string terminates loop, which processes each input string in turn
           {
-            StringTokenizer word = new StringTokenizer(input);
-            String[] wordCollection = new String[word.countTokens()];
+            StringTokenizer word = new StringTokenizer(input);          // break up input into sequence of its constituent words
+            String[] wordCollection = new String[word.countTokens()];   // stores words as they are encountered
 
-            int numberOfWords = 0;
-            int[] wordCount = new int [words.countTokens()];
-            int i;
+            int numberOfWords = 0;                // number of distinct words encountered so far
+            int[] wordCount = new int [words.countTokens()];      // wordCount[i] is number of occurrences-1 of wordCollection[i] in line
+            int i;    // general-purpose index used to step through the entires of wordCollection in the search for match with word, and to display words and counts
 
-            while (words.hasMoreTokends())
+            while (words.hasMoreTokends())            // keep going while there are still words left in the line to process
               {
-                String word = words.nextToken();
-                wordCollection[numberOfWords] = words;
-                for (i = 0; !wordCollection[i].equals(word); i++);
-                if (i == numberOfWords) numberOfWords++;
-                else wordCount[i]++;
-              }
+                String word = words.nextToken();          // word is current word of line we are processing
+                wordCollection[numberOfWords] = words;    // put word at the end to guarantee loop termination, since we're guaranteed to find a match
+                for (i = 0; !wordCollection[i].equals(word); i++);    // search wordCollection to determine if word is already stored in it
+                if (i == numberOfWords) numberOfWords++;        // word wasn't there previously, now it's in because we inserted 2 lines above
+                else wordCount[i]++;          // word was found there, so increment counter that tracks number of occurrences of word in line
+              }                                               // end of while that processes all words in line
 
-            for (i = 0; i<numberOfWords; i++)
-              System.out.print(wordCollection[i] + " " + (wordCount[i]+1) + (i<numberOfWords-1 ? "":"\n"));
+            for (i = 0; i<numberOfWords; i++)           // display contents of wordCollection and wordCount
+              System.out.print(wordCollection[i] + " " + (wordCount[i]+1) + (i<numberOfWords-1 ? "":"\n"));       // "\n" at end
 
             // prompt user and get next input string
             System.out.print("\nEnter a string of character("+ (SENTINEL.isEmpty() ? "enter" : ("\'" + SENTINEL + "\'")) + "to quit):");
             input = keyboardLine();
-          }
-      }
-  }
+          }           // end while
+      }               // end main
+  }                   // end class
