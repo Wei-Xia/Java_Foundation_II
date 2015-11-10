c65c1f34f1b4c5379dbbee26e110208a61421d5a
Wei Xia <xw900812@gmail.com>
2015-11-10 10:49:28 -0500

    Assignment #4

--- Assignment/Assignment #4/WordCount.java
+++ Assignment/Assignment #4/WordCount.java
@@ -5,8 +5,8 @@ class WordCount
     /* Prompt user for string and display word count, until user enters sentinel */
     public static void main(String[] args)
       {
-        final String SENTINEL = "";
-        Scanner keyboard = new Scanner(System.in);
+        final String SENTINEL = "";         // sentinel string that terminates program
+        Scanner keyboard = new Scanner(System.in);      // Scanner object wrapped around the keyboard, for user input
 
         // Prompt user and get first input string
         System.out.print("\nEnter a string of character("+ (SENTINEL.isEmpty() ? "enter" : ("\'" + SENTINEL + "\'")) + "to quit):");
@@ -20,7 +20,24 @@ class WordCount
             String[] wordCollection = new String[word.countTokens()];
 
             int numberOfWords = 0;
-            int[] WordCount = new int [words.countTokens()];
+            int[] wordCount = new int [words.countTokens()];
+            int i;
+
+            while (words.hasMoreTokends())
+              {
+                String word = words.nextToken();
+                wordCollection[numberOfWords] = words;
+                for (i = 0; !wordCollection[i].equals(word); i++);
+                if (i == numberOfWords) numberOfWords++;
+                else wordCount[i]++;
+              }
+
+            for (i = 0; i<numberOfWords; i++)
+              System.out.print(wordCollection[i] + " " + (wordCount[i]+1) + (i<numberOfWords-1 ? "":"\n"));
+
+            // prompt user and get next input string
+            System.out.print("\nEnter a string of character("+ (SENTINEL.isEmpty() ? "enter" : ("\'" + SENTINEL + "\'")) + "to quit):");
+            input = keyboardLine();
           }
       }
   }
