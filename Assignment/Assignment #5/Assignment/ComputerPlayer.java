5b13ee39d9b54852c519332f45219ff75c2c83d4
Wei Xia <xw900812@gmail.com>
2015-11-15 12:55:30 -0500

    Assignment #5

--- Assignment/Assignment #5/Assignment/ComputerPlayer.java
+++ Assignment/Assignment #5/Assignment/ComputerPlayer.java
@@ -1,31 +1,29 @@
 /**
-	ComputerPlayer is the AI client for computer, user can play with this
-		"smart" computer.
+	ComputerPlayer is the AI client for computer, 
+	user can play with this "smart" computer.
 */
 
 class ComputerPlayer
 {
-	public static int [] makeMove(int theBoard [] [], int turn)
+	public static int [] makeMove(int board [] [], int turn)
 	{
 		int square = 5;
-		int choice [] = new int [2];
+		int move [] = new int [2];
 
 		if(turn==1) // first move is to get 5
 		{
 			square = 5;
-			choice = squareConvert(square);
-			return choice;
-			}
-
-		choice = randomMove(theBoard); // make a random move.
-		return choice;
+			move = Convert(square);
+			return move;
 		}
 
+		move = randomMove(board); // make a random move.
+		return move;
+	}
 
-
-	public static int [] randomMove(int theBoard [] [] )
+	public static int [] randomMove(int board [] [] )
 	{
-		int choice [] = new int [2];
+		int move [] = new int [2];
 		int randomRow;
 		int randomCol;
 
@@ -33,34 +31,30 @@ class ComputerPlayer
 		{
 			randomRow = (int) (Math.random()*3 );
 			randomCol = (int) (Math.random()*3 );
-			if(theBoard [randomRow] [randomCol] == 0) break;
+			if(board [randomRow] [randomCol] == 0) 
+				break;
 		}
-		choice [0] = randomRow;
-		choice [1] = randomCol;
+		move [0] = randomRow;
+		move [1] = randomCol;
 
-		return choice;
+		return move;
 	}
 
-	/*
-	 * squareConvert takes in a square (1-9) and converts it to a row and column
-	 * Recieve: square, and int (1-9)
-	 * Returns: choice [], a 2D array with the row and column of the choice
-	 *
-	 */
-	public static int [] squareConvert(int square)
+// 	Convert will convert square (1-9) into a row and column
+	public static int [] Convert(int square)
 	{
-		int choice [] = new int [2];
+		int move [] = new int [2];
 
-		if(square == 1) {choice [0] = 0; choice[1] = 0;}
-		else if(square == 2) {choice [0] = 0; choice[1] = 1;}
-		else if(square == 3) {choice [0] = 0; choice[1] = 2;}
-		else if(square == 4) {choice [0] = 1; choice[1] = 0;}
-		else if(square == 5) {choice [0] = 1; choice[1] = 1;}
-		else if(square == 6) {choice [0] = 1; choice[1] = 2;}
-		else if(square == 7) {choice [0] = 2; choice[1] = 0;}
-		else if(square == 8) {choice [0] = 2; choice[1] = 1;}
-		else if(square == 9) {choice [0] = 2; choice[1] = 2;}
+			 if(square == 1) {move [0] = 0; move[1] = 0;}
+		else if(square == 2) {move [0] = 0; move[1] = 1;}
+		else if(square == 3) {move [0] = 0; move[1] = 2;}
+		else if(square == 4) {move [0] = 1; move[1] = 0;}
+		else if(square == 5) {move [0] = 1; move[1] = 1;}
+		else if(square == 6) {move [0] = 1; move[1] = 2;}
+		else if(square == 7) {move [0] = 2; move[1] = 0;}
+		else if(square == 8) {move [0] = 2; move[1] = 1;}
+		else if(square == 9) {move [0] = 2; move[1] = 2;}
 
-		return choice;
+		return move;
 	}
 }
