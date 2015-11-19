6d270c88e6a272b9a22239c851616eea3cdb817d
Wei Xia <xw900812@gmail.com>
2015-11-19 00:14:09 -0500

    Assignment #6

--- Assignment/Assignment #6/TicTacToe.java
+++ Assignment/Assignment #6/TicTacToe.java
@@ -1,11 +1,12 @@
 import java.io.*;
 
-class TicTacToe3
+class TicTacToe
 {
 	public static void main(String[] args)
 	{
 		new InputStreamReader(System.in);
-		BufferedReader theKeyboard = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader theKeyboard =
+			new BufferedReader(new InputStreamReader(System.in));
 
 		Board Game = new Board();
 
@@ -87,7 +88,8 @@ class TicTacToe3
 			// Player O's turn
       char[] canWin=Game.canWin();
       if(canWin[0]=='X') //If X could win with his next move
-      	System.out.println("Watch out! Player X can win if he plays in cell "+(int)Game.canWin()[1]+"!");
+      	System.out.println("Be careful! Player X can win " +
+								"if he/she plays in square " +(int)Game.canWin()[1]+ "!");
         System.out.print("Player O, Enter 1-9 to make choice: ");
 
 			while(true)
@@ -124,7 +126,8 @@ class TicTacToe3
 	public static int[] getMove()
 	{
 		new InputStreamReader(System.in);
-		BufferedReader theKeyboard = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader theKeyboard =
+							new BufferedReader(new InputStreamReader(System.in));
 
 		String input = "";
 		int [] move = new int[2];
@@ -216,7 +219,7 @@ class Board
 	'O' means O won;
 	'C' means a C's game.
 	*/
-	public char win()
+	char win()
 	{
 		char winner = 'N';
 		int catCheck = 1;
@@ -288,105 +291,121 @@ class Board
 		return winner;
 	}
 
-	public char [] canWin(){
+	char [] canWin()
+	{
     char winner = 'N';
     int catCheck = 1;
     char [] result=new char[2];
-    for(int i=1; i<10; i++){
-        winner = 'N';
-        //Places an X if a cell is not occupied
-        if(!elementMarked(ComputerPlayer.Convert(i)[0], ComputerPlayer.Convert(i)[1]))
-            markFirst(ComputerPlayer.Convert(i)[0],ComputerPlayer.Convert(i)[1]);
-        else //If cell is occupied, skip this iteration
-            continue;
 
-        // Check the columns
-        for (int column = 0; column < 3; column++) {
-            int accumulation = myBoard[0][column] * myBoard[1][column] * myBoard[2][column];
+		for(int i=1; i<10; i++)
+		{
+			winner = 'N';
+      //Places an X if a cell is not occupied
+      if(!elementMarked(ComputerPlayer.Convert(i)[0],ComputerPlayer.Convert(i)[1]))
+      	markFirst(ComputerPlayer.Convert(i)[0],ComputerPlayer.Convert(i)[1]);
+      else //If cell is occupied, skip this iteration
+        continue;
 
-            if (accumulation == 8) // 2*2*2 = 8, a win for X
-            {
-                winner = 'X';
-                break;
-            }
-            if (accumulation == 1) // 1*1*1 = 1, a win for O
-            {
-                winner = 'O';
-                break;
-            }
-        }
+      // Check the columns
+      for (int column = 0; column < 3; column++)
+			{
+          int accumulation = myBoard[0][column] *
+														 myBoard[1][column] * myBoard[2][column];
 
-        if (winner != 'N'){
-            result[0] = winner; //register winner
-            result[1]=(char)i;  //register cell that led to win
-            myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0; //undoing the cell selection
-            return result;
-        }
+          if (accumulation == 8) // 2*2*2 = 8, a win for X
+          {
+              winner = 'X';
+              break;
+          }
+          if (accumulation == 1) // 1*1*1 = 1, a win for O
+          {
+              winner = 'O';
+              break;
+          }
+    	}
 
-        // Check the rows
-        for (int row = 0; row < 3; row++) {
-            int accumulation = myBoard[row][0] * myBoard[row][1] * myBoard[row][2];
-            if (accumulation == 8) {
-                winner = 'X';
-                break;
-            }
-            if (accumulation == 1) {
-                winner = 'O';
-                break;
-            }
-        }
+			if (winner != 'N')
+			{
+				result[0] = winner; //assume the winner
+      	result[1]=(char)i;  //assume this square that will win
+				//undo all the behavior before
+      	myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0;
+      	return result;
+    	}
 
-        if (winner != 'N'){
-            result[0] = winner; //register winner
-            result[1]=(char)i;  //register cell that led to win
-            myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0; //undoing the cell selection
-            return result;
-        }
+    	// Check the rows
+    	for (int row = 0; row < 3; row++)
+			{
+    		int accumulation = myBoard[row][0] * myBoard[row][1] * myBoard[row][2];
 
-        // Check one diagonal
-        int accumulation = myBoard[0][0] * myBoard[1][1] * myBoard[2][2];
-        if (accumulation == 1)
-            winner = 'O';
-        if (accumulation == 8){
-            winner = 'X';
-            result[0] = winner; //register winner
-            result[1]=(char)i;  //register cell that led to win
-            myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0; //undoing the cell selection
-            return result;
-        }
+				if (accumulation == 8)
+				{
+      		winner = 'X';
+        	break;
+      	}
+      	if (accumulation == 1)
+				{
+        	winner = 'O';
+        	break;
+      	}
+    	}
 
-        // Check the other diagonal
-        accumulation = myBoard[0][2] * myBoard[1][1] * myBoard[2][0];
-        if (accumulation == 1)
-            winner = 'O';
-        if (accumulation == 8){
-            winner = 'X';
-            result[0] = winner; //register winner
-            result[1]=(char)i;  //register cell that led to win
-            myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0; //undoing the cell selection
-            return result;
-        }
+    	if (winner != 'N')
+			{
+    		result[0] = winner; //assume the winner
+      	result[1]=(char)i;  //assume this square that will win
+				//undo all the behavior before
+      	myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0;
+      	return result;
+    	}
 
-        // If nobody's won, Check for a cat's game
-        if (winner == 'N') {
-            for (int row = 0; row < 3; row++) {
-                for (int column = 0; column < 3; column++) {
-                    catCheck *= myBoard[row][column];
-                }
-            }
-            // any empty space is a zero. So product is zero if there is space
-            // left.
-            if (catCheck != 0)
-                winner = 'C';
-        }
+    	// Check one diagonal
+    	int accumulation = myBoard[0][0] * myBoard[1][1] * myBoard[2][2];
+    	if (accumulation == 1)
+    		winner = 'O';
+    	if (accumulation == 8)
+			{
+      	winner = 'X';
+    		result[0] = winner; //assume the winner
+      	result[1]=(char)i;  //assume this square that will win
+				//undo all the behavior before
+      	myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0;
+      	return result;
+    	}
 
-        result[0] = winner; //register winner
-        result[1]=(char)i;  //register cell that led to win
-        myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0; //undoing the cell selection
-    }
-    return result;
+    	// Check the other diagonal
+    	accumulation = myBoard[0][2] * myBoard[1][1] * myBoard[2][0];
+    	if (accumulation == 1)
+    		winner = 'O';
+    	if (accumulation == 8)
+			{
+      	winner = 'X';
+      	result[0] = winner; //assume the winner
+      	result[1]=(char)i;  //assume this square that will win
+				//undo all the behavior before
+      	myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0;
+      	return result;
+    	}
+
+    	// If nobody's won, Check for a cat's game
+    	if (winner == 'N')
+			{
+    		for (int row = 0; row < 3; row++)
+      		for (int column = 0; column < 3; column++)
+        		catCheck *= myBoard[row][column];
+
+      	if (catCheck != 0)
+      		winner = 'C';
+    	}
+
+    	result[0] = winner; //assume the winner
+    	result[1]=(char)i;  //assume this square that will win
+			//undo all the behavior before
+    	myBoard[ComputerPlayer.Convert(i)[0]][ComputerPlayer.Convert(i)[1]] = 0;
+		}
+
+    	return result;
 }
-
 
 	//toString enables printing out of the board
 	public String toString()
