94482ea234e4b9da8111c9e52de33d9b89d50090
Wei Xia <xw900812@gmail.com>
2015-12-02 10:33:09 -0500

    Assignment #8

---
+++ Assignment/Assignment #8/IdentifierXREF.java
@@ -0,0 +1,32 @@
+/**
+How it works?
+1. Procceed line by line
+2. Output line with blank space pre-appended
+3. Simplify each line to a string of words separeated by blanks
+4. For each word extracted, add/update word to grow XREF
+5. When all lines have been Procceed, then alphabetize XREF output to a file
+*/
+
+class IdentifierXREF
+{
+  public static void main(String[] args)
+  {
+    // Produce cross-reference to Java program
+    for (/*each line of input*/)
+    {
+      /*
+        Get next line;
+        Output it with line blank pre-appended;
+        Simplify line;
+      */
+
+      for (/* each word in line*/)
+      {
+          /* add/update word to XREF*/
+      }
+    }
+
+    // Alphabetize XREF;
+    // Output XREF;
+  }     // End of main
+}       //End of class
