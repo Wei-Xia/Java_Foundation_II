158c7294de36a24f7f2a54f6f52b8a6d9b09cb12
Wei Xia <xw900812@gmail.com>
2015-12-07 09:56:42 -0500

    Assignment #8

--- Assignment/Assignment #8/IdentifierXREF.java
+++ Assignment/Assignment #8/IdentifierXREF.java
@@ -15,26 +15,30 @@ class IdentifierXREF
   public static void main(String[] args) throws IOException
   {
     // Produce cross-reference to Java program
-    for (/*each line of input*/)
+
+    Scanner inputFile = new Scanner(new File(args[0] + ".java"));    // input file
+
+    Bufferedwriter outputFile = new Bufferedwriter(new FileWriter(args[0] + ".xref"));    // output file
+
+    for (int lineNumber = 1; inputFile.hasNextLine(); lineNumber++ /*each line of input*/)
     {
       /*
         Get next line;
         Output it with line blank pre-appended;
         Simplify line;
       */
-      Scanner inputFile = new Scanner(new File(args[0] + ".java"));    // input file
 
-      Bufferedwriter outputFile = new Bufferedwriter(new FileWriter(args[0] + ".xref"));    // output file
+      String line = inputFile.nextLine();
 
-      for (int lineNumber = 1; inputFile.hasNextLine(); lineNumber++)  //each word in line
+      outputFile.write(lineNumber + "   " + line);      // way one
+      outputFile.newLine();         // way two turn the line
+
+      StringTokenizer words = new StringTokenizer(simplifyLine(line));
+      
+      for ()  //each word in line
       {
           /* add/update word to XREF*/
-          String line = inputFile.nextLine();
 
-          outputFile.write(lineNumber + "   " + line);      // way one
-          outputFile.newLine();         // way two turn the line
-
-          StringTokenizer words = new StringTokenizer(simplifyLine(line));
       }
     }
 
