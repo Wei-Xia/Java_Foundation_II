f6f7c0c88159e001379e04e16d512cc890892cc2
Wei Xia <xw900812@gmail.com>
2015-12-08 09:57:30 -0500

    Assignment #8

--- Assignment/Assignment #8/Xref.java
+++ Assignment/Assignment #8/Xref.java
@@ -155,4 +155,32 @@ class Xref
     return output;
   }
 
+  void add(String word, int lineNumber)
+  {
+    /* If not a Java reserved word, insert word into the database,
+       indicating that it is referenced on line lineNumber */
+
+    // all Java reserved words; word at end guarantees
+    String[] javaReservedWord = {"abstract", "assert", "boolean", "break",
+            "byte", "case", "catch", "char", "class", "const", "continue",
+            "default", "do", "double", "else", "enum", "extends", "false",
+            "final", "finally", "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "interface", "long", "native",
+            "new", "null", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super", "switch",
+            "synchronized", "this", "throw", "throws", "transient", "true",
+            "try", "void", "volatile", "while", word};
+
+    int i;        // termination of for loop
+
+    // traverse javaReservedWord and see if any entry matches word
+    for (i=0; !word.equals(javaReservedWord[i]); i++);
+
+    // matches a reserved word, so don't add to database
+    if ( i<javaReservedWord.length -1)  return;
+
+    /* doesn't match a reserved word, so proceed by searching to see if
+          word is already in database */
+  }
+
 }       //End of class
