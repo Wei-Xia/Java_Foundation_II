d7450fdefd43e86a797fcdbc7e2795c067d8e714
Wei Xia <xw900812@gmail.com>
2015-12-09 16:28:48 -0500

    Chapter 8

---
+++ Textbook/Chapter 8/TextBook/Instructor.java
@@ -0,0 +1,14 @@
+/**
+  This class stores data about an instructor.
+*/
+
+public class instructor
+{
+  private String lastName;      // Last name
+  private String firstName;     // First name
+  private String officeNumber;  // Office number
+
+  /**
+    This constructor initialized the last name, first name, and office number.
+    
+}
