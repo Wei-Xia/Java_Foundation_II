67eb7765d9693958a25bb80743fa431a5acdeef4
Wei Xia <xw900812@gmail.com>
2015-12-16 16:07:25 -0500

    Chapter 8

---
+++ Textbook/Chapter 8/Course/Course.java
@@ -0,0 +1,10 @@
+/**
+  This class stores data about a course.
+*/
+
+public class Course
+{
+  private String courseName;      // Name of the course
+  private Instructor instructor;  // The instructor
+  private TextBook textBook;      // The textbook
+}
