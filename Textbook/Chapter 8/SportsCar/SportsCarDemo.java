50608ef9179d65b84a0465c6153be04c3aa73b3b
Wei Xia <xw900812@gmail.com>
2015-12-18 16:30:29 -0500

    Chapter 8

---
+++ Textbook/Chapter 8/SportsCar/SportsCarDemo.java
@@ -0,0 +1,15 @@
+/**
+  This program demonstrates the SportsCar class
+*/
+
+public class SportsCarDemo
+{
+  public static void main(String[] args)
+  {
+      // Create a SportsCar object
+      SportsCar yourNewCar = new SportsCar(CarType.PORSCHE, CarColor.RED, 10000);
+
+      // Display the object's values
+      System.out.println(yourNewCar);
+  }
+}
