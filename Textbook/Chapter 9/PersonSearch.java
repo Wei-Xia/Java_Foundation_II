1c20899f15f8e30ba6ee375cf316e8676f723dc6
Wei Xia <xw900812@gmail.com>
2015-12-23 14:39:08 -0500

    Chapter 9

---
+++ Textbook/Chapter 9/PersonSearch.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/**
+  This program uses the starsWith method to search using a partial string.
+*/
+
+public class PersonSearch
+{
+  public static void main(String[] args)
+  {
+      String lookUp;      // To hold a loopup string
+
+      // Create an array of names
+      String [] people = {"Cutshaw, Will", "Davis, George", "Davis, Jenny",
+                          "Russert, Phil", "Russell, Cindy", "Setzer, Charles",
+                          "Smathers, Holly", "Smith, Chris", "Smith, Brad",
+                          "Williams, Jean"};
+
+      // Create a Scanner object for keyboard input
+      Scanner keyboard = new Scanner (System.in);
+
+      // Get a partial name to search for
+      System.out.print("Enter the first few character of the last name to look up: ");
+      lookUp = keyboard.nextLine();
+
+      // Display all the names that begin with the string entered by the user.
+      System.out.println("Here are the names that match: ");
+      for (String person : people)
+      {
+          if (person.starsWith(lookUp))
+            System.out.println(person);
+      }
+  }
+}
