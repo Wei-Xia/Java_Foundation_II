bf58a70e1d55a34acc6055ce196ad343eadb72d3
Wei Xia <xw900812@gmail.com>
2016-01-03 21:34:49 -0500

    Chapter 9

---
+++ Textbook/Chapter 9/replaceString.java
@@ -0,0 +1,13 @@
+public class replaceString
+{
+  public static void main(String[] args)
+  {
+      String str1 = "Tom Talbert Tried Trains";
+      String str2;
+
+      str2 = str1.replace('T', 'D');
+
+      System.out.println(str1);
+      System.out.println(str2);
+  }
+}
