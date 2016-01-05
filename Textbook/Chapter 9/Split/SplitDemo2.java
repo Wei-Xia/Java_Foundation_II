13b2d68f0165f738a38ec60c8b786bdc2b779f9b
Wei Xia <xw900812@gmail.com>
2016-01-04 23:27:29 -0500

    Chapter 9

---
+++ Textbook/Chapter 9/Split/SplitDemo2.java
@@ -0,0 +1,15 @@
+public class SplitDemo2
+{
+  public static void main(String[] args)
+  {
+      // Create a string to tokenize.
+      String str = "joe@gaddisbooks.com";
+
+      // Get the tokens, using @ and . as delimiters
+      String[] tokens = str.split("[@.]");
+
+      // Display the tokens.
+      for ( String s : tokens)
+        System.out.println(s);
+  }
+}
