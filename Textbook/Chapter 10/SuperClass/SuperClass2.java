ea4e094e3c27a3c772410a2045ed022ab6279d5f
Wei Xia <xw900812@gmail.com>
2016-01-10 22:14:45 -0500

    Chapter 10

---
+++ Textbook/Chapter 10/SuperClass/SuperClass2.java
@@ -0,0 +1,20 @@
+public class SuperClass2
+{
+  /**
+    Constructor #1
+  */
+
+  public SuperClass2()
+  {
+    System.out.println("This is the superclass no-arg constructor.");
+  }
+
+  /**
+    Constructor #2
+  */
+
+  public SuperClass2()
+  {
+
+  }
+}
